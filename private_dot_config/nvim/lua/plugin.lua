49d056223bc302ce8f66830a39ff19c8b5e795cd
imcvampire <quocanh96@gmail.com>
2022-07-12 10:06:14 +0700

    feat(*): update

---
+++ private_dot_config/nvim/lua/plugin.lua
@@ -0,0 +1,126 @@
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          cmp.confirm()
+        end
+      else
+        fallback()
+      end
+    end, {"i","s","c",}),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+
+local lsp_installer = require "nvim-lsp-installer"
+
+-- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
+-- or if the server is already installed).
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    capabilities = capabilities,
+  }
+
+  -- if server.name == "tsserver" then
+  --     opts.root_dir = function() ... end
+  -- end
+
+  server:setup(opts)
+end)
+
+
+local saga = require 'lspsaga'
+
+saga.init_lsp_saga()
+
+require('lualine').setup {
+  options = {
+    theme = 'auto'
+  },
+  extensions = {'chadtree', 'fzf'},
+  sections = {
+    lualine_b = {
+      'branch',
+      'diff',
+      {
+        'diagnostics',
+
+        sources = { 'nvim_diagnostic', 'nvim_lsp', 'ale' },
+      }
+    }
+  }
+}
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
---
+++ private_dot_config/nvim/plugin.vim
@@ -0,0 +1,101 @@
+"dein Scripts-----------------------------
+if &compatible
+  set nocompatible               " Be iMproved
+endif
+
+" Required:
+let g:python3_host_prog="/usr/bin/python3"
+let g:python_host_prog="/usr/bin/python2"
+set runtimepath+=~/.dein/repos/github.com/Shougo/dein.vim
+
+call dein#begin('~/.dein')
+
+" Let dein manage dein
+" Required:
+call dein#add('~/.dein/repos/github.com/Shougo/dein.vim')
+
+call dein#add('neovim/nvim-lspconfig')
+call dein#add('williamboman/nvim-lsp-installer')
+call dein#add('glepnir/lspsaga.nvim')
+call dein#add('hrsh7th/nvim-cmp')
+call dein#add('hrsh7th/cmp-nvim-lsp')
+call dein#add('hrsh7th/cmp-buffer')
+call dein#add('hrsh7th/cmp-path')
+call dein#add('hrsh7th/cmp-cmdline')
+call dein#add('saadparwaiz1/cmp_luasnip')
+call dein#add('L3MON4D3/LuaSnip')
+call dein#add('Yggdroot/indentLine')
+call dein#add('editorconfig/editorconfig-vim')
+call dein#add('ConradIrwin/vim-bracketed-paste')
+call dein#add('scrooloose/nerdcommenter')
+call dein#add('jlanzarotta/bufexplorer')
+call dein#add('ctrlpvim/ctrlp.vim')
+call dein#add('ms-jpq/chadtree', {'rev': 'chad', 'build': 'python3 -m chadtree deps'})
+call dein#add('tpope/vim-surround')
+call dein#add('EdenEast/nightfox.nvim')
+call dein#add('nvim-lualine/lualine.nvim')
+call dein#add('kyazdani42/nvim-web-devicons')
+call dein#add('w0rp/ale')
+call dein#add('jiangmiao/auto-pairs')
+call dein#add('nvim-treesitter/nvim-treesitter', {'build': ':TSUpdate'})
+
+" Required:
+call dein#end()
+
+" Required:
+filetype plugin indent on
+syntax enable
+
+" If you want to install not installed plugins on startup.
+if dein#check_install()
+  call dein#install()
+endif
+"End dein Scripts-------------------------
+
+"indentLine
+let g:indentLine_char_list = ['|', '¦', '┆', '┊']
+let g:indentLine_enabled = 1
+let g:indentLine_concealcursor = "nv"
+
+" CHADtree
+let g:chadtree_settings = {
+  \  'ignore.name_exact': [".DS_Store", ".directory", "thumbs.db", ".git", ".idea"]
+  \ }
+autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | CHADopen
+nnoremap <leader>v <cmd>CHADopen<cr>
+
+" theme
+set termguicolors            " 24 bit color
+colorscheme duskfox
+
+" ale
+let g:ale_disable_lsp = 1
+
+function! LinterStatus() abort
+    let l:counts = ale#statusline#Count(bufnr(''))
+
+    let l:all_errors = l:counts.error + l:counts.style_error
+    let l:all_non_errors = l:counts.total - l:all_errors
+
+    return l:counts.total == 0 ? 'OK' : printf(
+    \   '%dW %dE',
+    \   all_non_errors,
+    \   all_errors
+    \)
+endfunction
+
+" nerdcommenter
+" Add spaces after comment delimiters by default
+let g:NERDSpaceDelims = 1
+
+" Use compact syntax for prettified multi-line comments
+let g:NERDCompactSexyComs = 1
+
+" lspsaga
+nnoremap <silent>K :Lspsaga hover_doc<CR>
+
+" scroll down hover doc or scroll in definition preview
+nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
+" scroll up hover doc
+nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
+
