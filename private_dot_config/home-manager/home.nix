db4eb6ab791598e9ec83aa6280ccea9f37e247e5
imcvampire <quocanh96@gmail.com>
2023-10-22 21:16:53 +0300

    feat: update

--- private_dot_config/home-manager/home.nix
+++ private_dot_config/home-manager/home.nix
@@ -21,10 +21,6 @@
     # lang
     pkgs.go
 
-    # ide
-    pkgs.emacs29-macport
-    pkgs.vscodium
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
