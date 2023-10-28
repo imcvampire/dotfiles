56084722d2ef020350107350eb817ee3c9c689da
imcvampire <quocanh96@gmail.com>
2023-10-29 00:30:30 +0300

    feat: update

--- private_dot_config/home-manager/home.nix
+++ private_dot_config/home-manager/home.nix
@@ -18,9 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # lang
-    pkgs.go
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
