be2a4d074f16d71e319680cf78ad6f125ab3d303
imcvampire <anhnq0@gmail.com>
2023-12-01 21:36:39 +0200

    feat: update

--- private_dot_config/home-manager/home.nix
+++ private_dot_config/home-manager/home.nix
@@ -30,6 +30,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    pkgs.pgcli
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
