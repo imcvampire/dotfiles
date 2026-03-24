abc91aae01e6abb0b58f77f2b877d0cb71463346
imcvampire <anhnq0@gmail.com>
2026-03-24 09:03:07 +0000

    update

--- private_dot_config/home-manager/hosts/nixos/default.nix
+++ private_dot_config/home-manager/hosts/nixos/default.nix
@@ -13,22 +13,33 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fi_FI.UTF-8";
+    LC_IDENTIFICATION = "fi_FI.UTF-8";
+    LC_MEASUREMENT = "fi_FI.UTF-8";
+    LC_MONETARY = "fi_FI.UTF-8";
+    LC_NAME = "fi_FI.UTF-8";
+    LC_NUMERIC = "fi_FI.UTF-8";
+    LC_PAPER = "fi_FI.UTF-8";
+    LC_TELEPHONE = "fi_FI.UTF-8";
+    LC_TIME = "fi_FI.UTF-8";
+  };
 
   networking.hostName = userConfig.hostname;
   networking.networkmanager.enable = true;
 
-  time.timeZone = "UTC"; 
-  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "UTC";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
   users.users.${userConfig.username} = {
     isNormalUser = true;
     extraGroups = ["networkmanager" "wheel" "docker"];
     shell = pkgs.zsh;
+    ignoreShellProgramCheck = true;
   };
-
-  programs.zsh.enable = true;
-
-  virtualisation.docker.enable = true;
 
   nix = {
     settings = {
@@ -48,4 +59,44 @@
       keep-derivations = true
     '';
   };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Budgie Desktop environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.desktopManager.budgie.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  virtualisation.docker.enable = true;
 }
